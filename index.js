d50241b81303dd757ddfab70e2d7f8954d88cf33
alexbotha <alexbotha@hotmail.co.uk>
2022-07-05 14:22:29 +0100

    commit tree 8cf80f25153ed8b5b9b684111d8c26724c16031c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+  return (`This Saturday, I want to ${activity}!`)
+}
+
+///////////////////////////////////////////////////////////////////////
+
+function mondayWork(monday = 'go to the office'){
+  return (`This Monday, I will ${monday}.`)
+}
+
+////////////////////////////////////////////////////////////////////////
+
+function wrapAdjective(result) {
+  return function wrapAdjective(emphatic) {
+    return `You are ${result}${emphatic}${result}!`
+  };
+}
+  
